2a224b7e5f18a2bfdef3d54f45bf0413df477f4d
leehyun2003 <mogui99@hufs.ac.kr>
2025-05-10 01:44:28 +0900

    Player Attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private StageData stageData;
+    [SerializeField]
+    private KeyCode keyCodeAttack = KeyCode.Space;
     private Movement2D movement2D;
+    private Weapon weapon;
 
     private void Awake(){
         movement2D = GetComponent<Movement2D>();
+        weapon = GetComponent<Weapon>();
     }
 
     private void Update(){
@@ -18,6 +22,13 @@ public class PlayerController : MonoBehaviour
         float y=Input.GetAxisRaw("Vertical");
         
         movement2D.MoveTo(new Vector3(x,y,0));
+
+        if(Input.GetKeyDown(keyCodeAttack)){
+            weapon.StartFiring();
+        }
+        else if(Input.GetKeyUp(keyCodeAttack)){
+            weapon.StopFiring();
+        }
     }
 
     private void LateUpdate(){
