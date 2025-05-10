3ec3ac93647867d3df0291304f21ace0d0940b83
whddnr1108 <jongwook1108@gmail.com>
2025-05-11 00:44:41 +0900

    scripts
    새로 변경된 scripts들입니다.

---
+++ Scripts/ButtonEvent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvent : MonoBehaviour
+{
+    
+    public void SceneLoader(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+ 
+}
